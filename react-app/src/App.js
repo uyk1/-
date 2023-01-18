04a9d3fd502464bdb7b32cd691f729b0ecc6577b
uyk1 <uyk0928@gmail.com>
2023-01-19 01:53:08 +0900

    08. Create 09. Update 10. Delete

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -33,30 +33,116 @@ function Nav(props) {
   </ol>
 </nav>
 }
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type='text' name="title" placeholder='title'/></p>
+      <p><textarea name='body' placeholder='body'></textarea></p>
+      <p><input type='submit' value='Create'/></p>
+    </form>
+  </article>
+}
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onUpdate(title, body);
+    }}>
+      <p><input type='text' name="title" placeholder='title' value={title} onChange={event=>{
+        setTitle(event.target.value);
+      }}/></p>
+      <p><textarea name='body' placeholder='body' value={body} onChange={event=>{
+        setBody(event.target.value);
+      }}></textarea></p>
+      <p><input type='submit' value='Update'/></p>
+    </form>
+  </article>
+}
 function App() {
   // const _mode = useState('WELCOME'); //상태관리를 위해 훅 사용
   // const mode = _mode[0]; //이를 통해 상태의 '값'을 읽어낼 수 있다.
   // const setMode = _mode[1]; //이는 함수를 의미. 이를 이용해 mode의 값을 바꿀 수 있다.
   const [mode, setMode] = useState('WELCOME'); // 위의 세 줄과 같은 의미 
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
-  ]
+  ]);
   let content = null;
+  let contextControl = null; //맥락적으로 만들어지는 UI
   if(mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB"></Article>
   } else if(mode === 'READ') {
     let title, body = null;
     for(let i=0; i < topics.length; i++) {
-      console.log(topics[i].id, id);
       if(topics[i].id === id) {
         title = topics[i].title;
         body = topics[i].body;
       }
     }
     content = <Article title={title} body={body}></Article>
+    contextControl = <>
+    <li><a href={'/update/'+id} onClick={Event=>{
+      Event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>
+    <li><input type='button' value='Delete' onClick={()=>{
+      const newTopics = [];
+      for(let i=0; i<topics.length; i++) {
+        if(topics[i].id != id) {
+          newTopics.push(topics[i]);
+        }
+      }
+      setTopics(newTopics);
+      setMode('WELCOME');
+    }} />
+
+    </li>
+    </>
+  } else if(mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body};
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
+  } else if(mode === 'UPDATE') {
+    let title, body = null;
+    for(let i=0; i < topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      const newTopics = [...topics];
+      const updatedTopic = {id:id, title:title, body:body};
+      for(let i = 0; i < newTopics.length; i++) {
+        if(newTopics[i].id === id){
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
   }
   return (
     <div>
@@ -68,6 +154,15 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <ul>  
+        <li>
+          <a href="/create" onClick={event => {
+            event.preventDefault();
+            setMode('CREATE');
+          }}>Create</a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
